3caa82c46b9193ad182774d1c6bea2ac6d556492
Николай <33680669+korapuzz@users.noreply.github.com>
2024-11-15 14:28:27 +0300

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,7 @@
-print(100)
-
-print(100)
-
-
-print(100)
-
+example = "Transaction"
+print(example[0])
+print(example[-1])
+print(example[len(example) // 2:])
+print(example[::-1])
+print(example[::2]) # нечетный
+print(example[1::2]) # четный
