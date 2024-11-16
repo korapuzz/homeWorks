c62a44eec1c59842b8e3a6d234d0e7fe31803f78
Николай <33680669+korapuzz@users.noreply.github.com>
2024-11-16 08:55:54 +0300

    Create module_1_01.py

---
+++ module_1_01.py
@@ -0,0 +1,6 @@
+tasks_count = 12
+all_time = 1.5
+curs_name = "Python"
+time_on_one_task = all_time / tasks_count
+print(f"Курс:{curs_name}, всего задач:{tasks_count}, затрачено часов: {all_time}, "
+      f"среднее время выполнения {time_on_one_task} часа.")
