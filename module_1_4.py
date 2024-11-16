7cd1be7efd6197abb7534cfffbbdd79e3d561eda
Николай <33680669+korapuzz@users.noreply.github.com>
2024-11-16 08:56:57 +0300

    Original tast module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,9 @@
-tasks_count = 12
-all_time = 1.5
-curs_name = "Python"
-time_on_one_task = all_time / tasks_count
-print(f"Курс:{curs_name}, всего задач:{tasks_count}, затрачено часов: {all_time}, "
-      f"среднее время выполнения {time_on_one_task} часа.")
+my_string = input("Введите строку: ")
+
+print("Результаты:")
+print("Длина строки:", len(my_string), "символов")
+print("Строка в верхнем регистре:", my_string.upper())
+print("Строка в нижнем регистре:", my_string.lower())
+print("Строка без пробелов :", my_string.replace(" ",""))
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
