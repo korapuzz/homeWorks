0fd611c780289dc0fe7360548b6d18d75deba9e3
Николай <33680669+korapuzz@users.noreply.github.com>
2024-11-18 12:16:24 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,25 @@
-example = "Transaction"
-print(example[0])
-print(example[-1])
-print(example[len(example) // 2:])
-print(example[::-1])
-print(example[::2]) # нечетный
-print(example[1::2]) # четный
+immutable_var = (1, 'two', True, (1, 2, 3), [1, 2, 3], {1, 2, 3})
+
+print("Вывод кортежа")
+print(immutable_var)
+print("Вывод элементов кортежа")
+print(*immutable_var)
+print("Вывод элемента кортежа")
+print(immutable_var[2])
+
+# Так делать нельзя
+# immutable_var[5] = 4
+
+mutable_list = [1, 'two', True, (1, 2, 3), [1, 2, 3], {1, 2, 3}]
+
+print("Вывод списка")
+print(mutable_list)
+print("Вывод элементов списка")
+print(*mutable_list)
+print("Вывод элемента списка")
+print(mutable_list[2])
+
+# Так делать можно
+mutable_list[5] = 4
+print("Вывод обновленного списка")
+print(mutable_list)
