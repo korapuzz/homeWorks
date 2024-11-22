acd4c3b9deffce3a6662204265d67f3dfe1db5d0
Николай <33680669+korapuzz@users.noreply.github.com>
2024-11-22 14:50:55 +0300

    Update module_2_if.py

--- module_2_if.py
+++ module_2_if.py
@@ -1,9 +1,13 @@
-first = int(input("Введите первое  число: "))
-second = int(input("Введите второе число: "))
-third = int(input("Введите третье число: "))
-if first == second == third:
-    print(3)
-elif first == second or first == third or second == third:
-    print(2)
+try:
+    first = int(input("Введите первое  число: "))
+    second = int(input("Введите второе число: "))
+    third = int(input("Введите третье число: "))
+except ValueError:
+    print("Пожалуйста, вводите только числа")
 else:
-    print(0)
+    if first == second == third:
+        print(3)
+    elif first == second or first == third or second == third:
+        print(2)
+    else:
+        print(0)
