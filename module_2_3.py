6fa6546a00d1edb708d1847a306b096e7c6e4958
Николай <33680669+korapuzz@users.noreply.github.com>
2024-11-24 22:37:48 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,11 +1,13 @@
 my_list = list(map(int,input("Введите числа через пробел: ").split()))
+my_list.append(-1)
 count = len(my_list)
-k = -1
-while k < count - 1 and my_list[k] >= 0:
-    k += 1
+k = 0
+while True:
     if my_list[k] < 0:
         break
     elif my_list[k] == 0:
+        k += 1
         continue
-
-    print(my_list[k])
+    else:
+        print(my_list[k])
+        k += 1
