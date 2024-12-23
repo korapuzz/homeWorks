c49155bf575e1fc47462ebfd543b01b69b295217
Николай <33680669+korapuzz@users.noreply.github.com>
2024-12-23 10:37:33 +0300

    Create classes_01.py

---
+++ classes_01.py
@@ -0,0 +1,17 @@
+class House:
+    def __init__ (self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        print(f"Строение {self.name} имеет {self.number_of_floors} этажей")
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for k in range (new_floor):
+                print(k + 1)
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
