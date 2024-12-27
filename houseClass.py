e9dca100eafef1773d3581fa0e57868046c8358d
Николай <33680669+korapuzz@users.noreply.github.com>
2024-12-27 11:28:18 +0300

    Update houseClass.py

--- houseClass.py
+++ houseClass.py
@@ -1,8 +1,18 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
     def __init__ (self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
         #print(f"Строение {self.name} имеет {self.number_of_floors} этажей")
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
 
     def __len__(self):
         return int(self.number_of_floors)
@@ -39,6 +49,7 @@ class House:
 
     def __le__(self, other):
         return not self.__gt__(other)
+
 
     def __ge__(self, other):
         return not self.__lt__(other)
