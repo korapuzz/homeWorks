0c9482b713e4f26d0b03235eacef6af588e58072
Николай <33680669+korapuzz@users.noreply.github.com>
2025-01-08 13:52:25 +0300

    Update module_06_final.py

--- module_06_final.py
+++ module_06_final.py
@@ -1,110 +1,106 @@
-from time import sleep
+from math import pi
+class Figure:
+    sides_count = 0
+    filled = False
 
-class User:
-    def __init__(self, nickname, password, age):
-        self.nickname = nickname
-        self.password = hash(password)
-        self.age = age
+    def __init__(self, color, *sides):
+        if self.__is_valid_color(*color):
+            self.__color = color
+        else:
+            self.__color = [0, 0, 0]
 
-    def __str__(self):
-        return self.nickname
+        if self.__is_valid_sides(*sides):
+            self.__sides = list(sides)
+        else:
+            self.__sides = [1] * self.sides_count
 
-    def __repr__(self):
-        return f"User(nickname={self.nickname}, age={self.age})"
+    def get_color(self):
+        return self.__color
 
-    def __eq__(self, other):
-        return self.age == other.age and self.nickname == other.nickname
+    def __is_valid_color(self, r, g, b):
+        return all(isinstance(i, int) and 0 <= i <= 255 for i in (r, g, b))
 
-class Video:
-    def __init__(self, title, duration, adult_mode=False):
-        self.title = title
-        self.duration = duration
-        self.time_now = 0
-        self.adult_mode = adult_mode
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
 
-    def __str__(self):
-        return self.title
+    def __is_valid_sides(self, *sides):
+        return len(sides) == self.sides_count and all(isinstance(side, int) and side > 0 for side in sides)
 
-    def __repr__(self):
-        return f"Video(title={self.title}, duration={self.duration}, adult_mode={self.adult_mode})"
+    def get_sides (self):
+        return self.__sides
 
-class UrTube:
-    def __init__(self):
-        self.users = []
-        self.videos = []
-        self.current_user = None
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
 
-    def log_in(self, nickname, password):
-        for user in self.users:
-            if user.nickname == nickname and user.password == self.hash(password):
-                self.current_user = user
-                return
-        print("Неверный логин или пароль")
+    def __len__(self):
+        return sum(self.__sides) # должен возвращать периметр фигуры.
 
-    def register(self, nickname, password, age):
-        for user in self.users:
-            if user.nickname == nickname:
-                print(f"Пользователь {nickname} уже существует")
-                return
-        new_user = User(nickname, password, age)
-        self.users.append(new_user)
-        self.current_user = new_user
+class Circle(Figure):
+    sides_count = 1
 
-    def log_out(self):
-        self.current_user = None
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.__radius = self.__calculate_radius()
 
-    def add(self, *videos):
-        for video in videos:
-            if not any(v.title == video.title for v in self.videos):
-                self.videos.append(video)
+    def __calculate_radius(self):
+        return self.get_sides()[0] / (2 * pi)
 
-    def get_videos(self, search_word):
-        search_word = search_word.lower()
-        return [video.title for video in self.videos if search_word in video.title.lower()]
+    def get_square(self):
+        radius = self.__calculate_radius()
+        return pi * (radius ** 2)
 
-    def watch_video(self, title):
-        if not self.current_user:
-            print("Войдите в аккаунт, чтобы смотреть видео")
-            return
+class Triangle(Figure):
+    sides_count = 3
 
-        video = next((v for v in self.videos if v.title == title), None)
-        if not video:
-            print("Видео не найдено")
-            return
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.__height = self.__calculate_height()
 
-        if video.adult_mode and self.current_user.age < 18:
-            print("Вам нет 18 лет! Пожалуйста, покиньте страницу!")
-            return
+    def __calculate_height(self):
+        sides = self.get_sides()
+        s = sum(sides) / 2
+        area = (s * (s - sides[0]) * (s - sides[1]) * (s - sides[2])) ** 0.5
+        base = sides[0]
+        return 2 * area / base
 
-        for second in range(video.time_now, video.duration):
-            print(second + 1, end = " ")
-            sleep(1)
+    def get_square(self):
+        sides = self.get_sides()
+        s = sum(sides) / 2
+        return (s * (s - sides[0]) * (s - sides[1]) * (s - sides[2])) ** 0.5
 
-        video.time_now = 0
-        print("Конец видео")
+class Cube(Figure):
+    sides_count = 12
 
-# Код для проверки:
-ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+    def __init__(self, color, *sides):
+        sides = [sides[0]] * self.sides_count if len(sides) == 1 else [1] * self.sides_count
+        super().__init__(color, *sides)
 
-# Добавление видео
-ur.add(v1, v2)
+    def get_volume(self):
+        # Volume of the cube = side^3, we assume all sides are equal
+        side = self.get_sides()[0]
+        return side ** 3
 
-# Проверка поиска
-print(ur.get_videos('лучший'))
-print(ur.get_videos('ПРОГ'))
 
-# Проверка на вход пользователя и возрастное ограничение
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-ur.watch_video('Для чего девушкам парень программист?')
+#Код для проверки:
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
 
-# Проверка входа в другой аккаунт
-ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-print(ur.current_user)
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
 
-# Попытка воспроизведения несуществующего видео
-ur.watch_video('Лучший язык программирования 2024 года!')
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
