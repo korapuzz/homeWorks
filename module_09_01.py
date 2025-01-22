4792a5d0b7c53943641e2ab56979e1a72f038381
Николай <33680669+korapuzz@users.noreply.github.com>
2025-01-22 23:06:32 +0300

    Create module_09_01.py

---
+++ module_09_01.py
@@ -0,0 +1,12 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    try:
+        for func in functions:
+            results[func.__name__] = func(int_list)
+    except TypeError:
+        results = "Некорректные данные"
+    return results
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func([6, "20", 15, 9], max, min))
